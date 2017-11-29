572eb0efee570dbcb9527208b7af367e1cfc0223
Jonas Hecht <jonas.hecht@codecentric.de>
2017-11-29 15:18:20 +0100

    New step for Windows and Ansible

---
+++ day01/03_Ansible_Windows/Tutorial/Script.sh
@@ -0,0 +1,5 @@
+# Clone example application https://github.com/jonashackt/restexamples into Tutorial directory
+#
+# Build example application with Maven: mvn clean install
+#
+ansible-playbook SpringBootLinux.yml -i inventory --extra-vars "spring_boot_app_jar=restexamples/target/restexamples-0.0.1-SNAPSHOT.jar"
--- day01/4 Ansible Variablen/Tutorial/Scripts.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Gathering Facts
-ansible webservers -i inventory  -m setup
-# Filtering Facts
-ansible webservers -i inventory -m setup -a "filter=ansible_distribution"
-# Filtering Facts after creating custom ones
-ansible webservers -i inventory -m setup -a "filter=ansible_local"
